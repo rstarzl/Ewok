dc0c8ba0a1d3ce8234493d87e8501ff105e546e3
JS <num1ysc@gmail.com>
2014-10-05 22:43:28 -0400

    Crawer Driver is prepared to threading

--- Ewok/src/Ewok/CrawlerDriver.java
+++ Ewok/src/Ewok/CrawlerDriver.java
@@ -1,11 +1,24 @@
 package Ewok;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 
 
-public class CrawlerDriver {
+
+public class CrawlerDriver extends Thread{
+	private ExecutorService eservice;
+	
+	public CrawlerDriver(){
+		int nrOfProcessors = Runtime.getRuntime().availableProcessors();
+		eservice = Executors.newFixedThreadPool(nrOfProcessors);
+		// Q 3개 쓰레드로 돌릴꺼고,,,,,DB는 우선 Q안에 포함시키는 걸로.
+	}
+	
+	
+	@Override
 	public void run() {
-
+		
 	}
 }
