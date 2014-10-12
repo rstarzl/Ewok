810408a12e8f37039f00832cc338ebfe61ad613e
JS <num1ysc@gmail.com>
2014-10-11 22:01:10 -0400

    temp commit
    Conflicts:
    	Ewok/src/Ewok/Processor/TargetQueueProcessor.java

--- Ewok/src/Ewok/Processor/TargetQueueProcessor.java
+++ Ewok/src/Ewok/Processor/TargetQueueProcessor.java
@@ -34,12 +34,12 @@ public class TargetQueueProcessor extends QueueProcessor {
 //		entry.setSiteURL("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=100");
 //		entry.setDepth(GlobalContext.getDepthLimit());
 		
-		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=100"));
-		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101"));
-		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=102"));
-		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=103"));
-		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=104"));
-		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=105"));
+//		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=100"));
+//		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101"));
+//		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=102"));
+//		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=103"));
+//		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=104"));
+//		this.push(new QueueEntry("http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=105"));
 //		this.push(new QueueEntry("http://media.daum.net/society/all/#page=1&type=tit_cont"));
 //		this.push(new QueueEntry("http://media.daum.net/politics/all/#page=1&type=tit_cont"));
 //		this.push(new QueueEntry("http://media.daum.net/economic/all/#page=1&type=tit_cont"));
@@ -53,7 +53,10 @@ public class TargetQueueProcessor extends QueueProcessor {
 //		this.push(new QueueEntry("http://news.nate.com/recent?mid=n0501"));
 //		this.push(new QueueEntry("http://news.nate.com/recent?mid=n0601"));
 //		this.push(new QueueEntry("http://news.nate.com/recent?mid=n0701"));
-	}
+
+	
+		this.push(new QueueEntry("http://news.nate.com/recent?cate=pol&mid=n0201&type=c&date=20141008"));
+}
 	
 	@Override
 	public void push(QueueEntry entry){
@@ -62,6 +65,7 @@ public class TargetQueueProcessor extends QueueProcessor {
 			return;	// drop.
 		}
 		super.push(entry);
+//		GlobalContext.getURLDB().add(entry);
 	}
 	
 	/* @ modified by JS */
@@ -76,14 +80,14 @@ public class TargetQueueProcessor extends QueueProcessor {
 			QueueEntry	workingItem = this.pop();
 			if (workingItem != null){
 				// 1. Getting URL.
-				ArrayList <URLInfo> linkList = regionFilter.filter(workingItem);
-
+				ArrayList <URLInfo> urlResult = regionFilter.filter(workingItem);
+				ArrayList <URLInfo> linkList = (ArrayList <URLInfo>)urlResult.clone();
 				// 2. Assigning Work.
 				for (URLInfo workingURL : linkList){
 					QueueEntry	entry = new QueueEntry(workingURL.getUrl());
 					entry.setURLInfo(workingURL);
+					GlobalContext.logCommon("TP : " + entry.getSiteURL() + "\t" +entry.getUrlType() + "\t" + this.getQSize());
 					GlobalContext.getAvailableClassifierQL().push(entry);
-					GlobalContext.logCommon("TP : " + entry.getSiteURL());
 				}
 			}
 		}
