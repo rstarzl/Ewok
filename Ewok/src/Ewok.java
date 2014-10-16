1f878582ee6753cc4c4ba95ad2de49437de9606a
JS <num1ysc@gmail.com>
2014-10-15 21:03:53 -0400

    prepare to implement Rollback code,

--- Ewok/src/Ewok.java
+++ Ewok/src/Ewok.java
@@ -14,6 +14,13 @@ public class Ewok {
 		
 		CrawlerDriver cd = new CrawlerDriver();
 		
+		// Added for Q snap-shot
+		if (args.length > 0){
+			// TODO : DB rollback Code.
+			// TODO : 각 큐마다 스냅샷 찍을것,.
+		}
+			
+		
 		cd.run();
 	}
 }
